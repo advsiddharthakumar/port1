8d1ee2d84444ac5c2136be52f5bc4c9045685a4c
advsiddharthakumar <advsiddharthakumar@yahoo.com>
2024-11-30 22:27:00 +0530

    m

--- port/settings.py
+++ port/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-on#g_q7($pi_xi4um%)*#_o1=wnx@l8_!)$#4ysiu%@$6vykhk'
+SECRET_KEY = 'Linuxmint@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['advsiddharthakumar.vercel.app']
 
@@ -134,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
