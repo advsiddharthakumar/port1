54bc3eeeba8bf2ee8433901f2b933de1bf20ccdb
advsiddharthakumar <advsiddharthakumar@yahoo.com>
2024-12-16 17:33:38 +0530

    m

--- port/settings.py
+++ port/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Linuxmint_2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['advsiddharthakumar.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
